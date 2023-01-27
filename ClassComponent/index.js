6feb00edb7c97f60dc301cc6351c08b4988cb998
orotel <orotel.aaos@gmail.com>
2023-01-26 23:03:44 -0300

    add my first class component

--- ClassComponent/index.js
+++ ClassComponent/index.js
@@ -1,7 +1,11 @@
-class MeuHello extends React.Component{ 
+class Hello extends React.Component{ 
     render(){
         return(
-            <h1>Hello there </h1>
+            <div>
+                <h1>Hello</h1>
+            </div>
         )
     }
-}
+} 
+
+ReactDOM.render(<Hello/>,document.getElementById("root"))
